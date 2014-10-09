89d1ae60b9e8ac5e1db8bfbf802f725ac551b2ad
Matthew Farr <farr16@up.edu>
2014-10-09 01:51:04 +0000

    This should work

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
  *
  */
 #include "robot.h"
+#include "farr16.h"
 
 /**
  * main()
@@ -33,6 +34,8 @@ int main(int argc, const char * argv[])
   robPrintAscii();
 
   robPrintMessage(num);
+
+  farr16();
 
   return 0;
 }

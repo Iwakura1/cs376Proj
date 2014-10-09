0fadd86bc1c7879bba0c188c2ab5afad1dbf813a
Matthew Garand <garand17@ip-172-31-35-158.us-west-2.compute.internal>
2014-10-09 01:37:41 +0000

    created username function for <yourname> favorit food

---
+++ username.c
@@ -0,0 +1,7 @@
+/* this is file username.c */
+#include <stdio.h>
+#include "username.h"
+void username() {
+  //replace the following print statement with your favorite food.
+  printf("<username> likes metals with lots of nickel and  copper.\n"); 
+}
